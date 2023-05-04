f95f69ccf7c301c0686b617838cd847f1dfcb958
sigristarisa <arisasigrist.ch@gmail.com>
2023-05-04 11:19:16 +0200

    created the models

--- motd/mayonnaise/models.py
+++ motd/mayonnaise/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Mayonnaise(models.Model):
+    id = models.AutoField()
+    name = models.CharField()
+    ingredient = models.CharField()
+    portion = models.CharField()
+
+    def __str__(self):
+        return self.name
+    
+    
+class Dish(models.Model):
+    id = models.AutoField()
+    name = models.CharField()
+    image = models.CharField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Combination(models.Model):
+    id = models.AutoField()
+    mayonnaise = models.ForeignKey(Mayonnaise, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "combination – mayonnaise: {mayonnaise}, dish: {dish}".format(mayonnaise=self.mayonnaise, dish=self.dish)
--- motd/mayonnaise/views.py
+++ motd/mayonnaise/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def mayonnaise(request):
+    context = {id: 1}
+    return render(request, "mayonnaise/mayonnaise.html", context)
